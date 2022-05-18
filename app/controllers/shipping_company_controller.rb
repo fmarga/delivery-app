54959cb5dea2c39966d49c205286cc92cccf15a8
fmarga <marga.fmartins@gmail.com>
2022-05-17 22:10:46 -0300

    cria pagina de detalhes de uma transportadora

--- app/controllers/shipping_company_controller.rb
+++ app/controllers/shipping_company_controller.rb
@@ -1,5 +1,16 @@
 class ShippingCompanyController < ApplicationController
+  before_action :shipping_company_params, only: [:show]
+
   def index
     @shipping_companies = ShippingCompany.all
+  end
+
+  def show; end
+
+  
+  private
+
+  def shipping_company_params
+    @shipping_company = ShippingCompany.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :shipping_company, only: [:index]
+  resources :shipping_company, only: [:index, :show]
 end
