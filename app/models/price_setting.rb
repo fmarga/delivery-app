2d80a59431f696b4c94374879cc5f794793ae440
fmarga <marga.fmartins@gmail.com>
2022-05-21 19:14:02 -0300

    configura price_setting com i18n

--- app/models/price_setting.rb
+++ app/models/price_setting.rb
@@ -1,3 +1,5 @@
 class PriceSetting < ApplicationRecord
+  validates :min_volume, :max_volume, :min_weight, :max_weight, :distance_value, presence: true
+
   belongs_to :shipping_company
 end
--- spec/models/price_setting_spec.rb
+++ spec/models/price_setting_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe PriceSetting, type: :model do
+  # describe '#valid?' do
+  #   context 'presence' do
+  #     it 'falso quando o campo de volume mínimo estiver vazio' do
+  #       entregas = ShippingCompany.create(corporate_name: 'Transportes e Logística LTDA', brand_name: 'Entregas Já', registration_number: '22693970000152', email_domain: 'entregasja.com.br', address: 'Av do Transporte, 10', city: 'Porto Alegre', state: 'RS', postal_code: '96224-390')
+  #       price = PriceSetting.new(min_volume: 1, max_volume: 5, min_weight: 1, max_weight: 10, distance_value: 50, shipping_company: entregas)
+
+  #       result = price.valid?
+
+  #       expect(result).to eq false
+  #     end
+  #   end
+  # end
 end
