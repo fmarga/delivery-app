4013b4d53adb4edf1b011bfa891e887745d7b8c7
fmarga <marga.fmartins@gmail.com>
2022-05-23 17:19:35 -0300

    cria funcionalidade de criar uma conta como administrador

---
+++ spec/system/admin_access/admin_signs_up_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Administrador se cadastra' do
+  it 'com sucesso' do
+
+    visit root_path
+    click_on 'ENTRAR COMO ADMINISTRADOR'
+    click_on 'Criar uma nova conta'
+    fill_in 'Nome de usuário', with: 'fmarga'
+    fill_in 'E-mail', with: 'admin@gmail.com'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Criar conta'
+
+    expect(page).to have_content 'Boas vindas! Você realizou seu registro com sucesso.'
+    expect(page).to have_content 'fmarga'
+    expect(page).to have_button 'SAIR'
+  end
+end
