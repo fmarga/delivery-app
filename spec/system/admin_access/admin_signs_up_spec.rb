63fca1d796349241c48e1fdd0011e107e7b5e843
fmarga <marga.fmartins@gmail.com>
2022-05-26 16:20:44 -0300

    cria validação de email para admin

--- spec/system/admin_access/admin_signs_up_spec.rb
+++ spec/system/admin_access/admin_signs_up_spec.rb
@@ -14,4 +14,17 @@ describe 'Administrador se cadastra' do
     expect(page).to have_content 'Boas vindas! Você realizou seu registro com sucesso.'
     expect(page).to have_button 'SAIR'
   end
+
+  it 'com e-mail inválido' do
+    visit root_path
+    click_on 'ADMINISTRADOR'
+    click_on 'Criar uma nova conta'
+    fill_in 'E-mail', with: 'admin@gmail.com'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Criar conta'
+
+    expect(page).to have_content 'Não foi possível salvar administrador: 1 erro'
+    expect(page).to have_content 'E-mail não é válido'
+  end
 end
