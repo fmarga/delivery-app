26e123f8c010a7cf0eff478af47216d61bce1d70
fmarga <marga.fmartins@gmail.com>
2022-05-28 23:25:21 -0300

    cria teste de validação de email para admin

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
+  describe '#valid?' do
+    context 'format' do
+      it 'falso quando domínio de e-mail não é @sistemadefrete.com.br' do
+        admin = Admin.new(email: 'admin@outrodominio.com.br', password: 'password')
+
+        result = admin.valid?
+        
+        expect(result).to eq false
+      end
+    end
+  end
 end
